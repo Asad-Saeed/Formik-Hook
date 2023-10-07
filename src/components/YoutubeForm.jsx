ba13d65508df10b08c655a6087015f1ac7262848
Asad Saeed <asadsaeed12225@gmail.com>
2023-10-07 20:02:33 +0500

    form state handle and form submit using formik

--- src/components/YoutubeForm.jsx
+++ src/components/YoutubeForm.jsx
@@ -1,17 +1,28 @@
 import React from "react";
+import {useFormik} from 'formik'
 
 const YoutubeForm = () => {
+  const formik=useFormik({
+    initialValues:{
+      name:"",
+      email:"",
+      channel:""
+    },
+    onSubmit:values=>{
+      console.log("FormValues",values);
+    }
+  })
   return (
     <div className="main-container">
       <h1 className="heading">Formic Library</h1>
-      <form>
+      <form onSubmit={formik.handleSubmit}>
         <label htmlFor="name">Name:</label>
-        <input id="name" name="name" type="text" />
+        <input id="name" name="name" type="text" onChange={formik.handleChange} value={formik.values.name} />
         <label htmlFor="email">Email Address:</label>
-        <input id="email" name="email" type="email" />
+        <input id="email" name="email" type="email" onChange={formik.handleChange} value={formik.values.email} />
         <label htmlFor="channel">Channel Name:</label>
-        <input id="channel" name="channel" type="text" />
-        <button>Submit</button>
+        <input id="channel" name="channel" type="text" onChange={formik.handleChange} value={formik.values.channel} />
+        <button type="submit">Submit</button>
       </form>
     </div>
   );
